6d38ba6adbe3338aefaf4d1b5c15cc91d12fc199
阿部瑞樹 <abemizuki@abemizukinoMacBook-Air.local>
2024-08-03 07:53:29 +0900

    管理者側の会員管理機能を作ろう

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,18 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Restaurant;
-use App\Models\Category;
+// use App\Models\Restaurant;
+// use App\Models\Category;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        $highly_rated_restaurants = Restaurant::take(6)->get();
-        $categories = Category::all();
-        $new_restaurants = Restaurant::orderBy('created_at', 'desc')->take(6)->get();
+    //     $highly_rated_restaurants = Restaurant::take(6)->get();
+    //     $categories = Category::all();
+    //     $new_restaurants = Restaurant::orderBy('created_at', 'desc')->take(6)->get();
 
-        return view('home', compact('highly_rated_restaurants', 'categories', 'new_restaurants'));
-    }
+    //     return view('home', compact('highly_rated_restaurants', 'categories', 'new_restaurants'));
+    // }
+}
 }
