0ce7848ef2492483fb207298c8e16d9726b6733c
阿部瑞樹 <abemizuki@abemizukinoMacBook-Air.local>
2024-08-10 11:23:27 +0900

    commit

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,29 +1,41 @@
 @extends('layouts.app')
 
+@push('scripts')
+    <script src="{{ asset('/js/reservation-modal.js') }}"></script>
+@endpush
+
 @section('content')
-    <div class="col container">
+    <div class="container nagoyameshi-container pb-5">
         <div class="row justify-content-center">
-            <div class="col-xl-7 col-lg-8 col-md-9">
-                <nav class="mb-4" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <div class="col-xl-5 col-lg-6 col-md-8">
+                <nav class="my-3" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="{{ route('admin.users.index') }}">会員一覧</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">会員詳細</li>
+                        <li class="breadcrumb-item"><a href="{{ route('home') }}">ホーム</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">会員情報</li>
                     </ol>
                 </nav>
 
-                <h1 class="mb-4 text-center">{{ $user->name }}</h1>
+                <h1 class="mb-3 text-center">会員情報</h1>
+
+                <div class="d-flex justify-content-end align-items-end mb-3">
+                    <div>
+                        <a href="{{ route('user.edit', $user) }}">編集</a>
+                    </div>
+                </div>
+
+                @if (session('flash_message'))
+                    <div class="alert alert-info" role="alert">
+                        <p class="mb-0">{{ session('flash_message') }}</p>
+                    </div>
+                @endif
+
+                @if (session('error_message'))
+                    <div class="alert alert-danger" role="alert">
+                        <p class="mb-0">{{ session('error_message') }}</p>
+                    </div>
+                @endif
 
                 <div class="container mb-4">
-                    <div class="row pb-2 mb-2 border-bottom">
-                        <div class="col-3">
-                            <span class="fw-bold">ID</span>
-                        </div>
-
-                        <div class="col">
-                            <span>{{ $user->id }}</span>
-                        </div>
-                    </div>
-
                     <div class="row pb-2 mb-2 border-bottom">
                         <div class="col-3">
                             <span class="fw-bold">氏名</span>
@@ -111,22 +123,6 @@
                                     {{ $user->occupation }}
                                 @else
                                     未設定
-                                @endif
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="row pb-2 mb-2 border-bottom">
-                        <div class="col-3">
-                            <span class="fw-bold">会員種別</span>
-                        </div>
-
-                        <div class="col">
-                            <span>
-                                @if ($user->subscribed('premium_plan'))
-                                    有料会員
-                                @else
-                                    無料会員
                                 @endif
                             </span>
                         </div>
