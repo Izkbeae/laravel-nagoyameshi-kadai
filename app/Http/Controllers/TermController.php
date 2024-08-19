86ebfc2455cdffbcf3ff106019ee565d5dadc3e8
阿部瑞樹 <abemizuki@abemizukinoMacBook-Air.local>
2024-08-19 09:45:29 +0900

    commit

--- app/Http/Controllers/TermController.php
+++ app/Http/Controllers/TermController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Term;
 
 class TermController extends Controller
 {
-    //
+    // -----
+    // index アクション
+    // ------
+    public function index() {
+        $term = Term::first();
+        return view('terms.index', compact('term'));
+}
 }
